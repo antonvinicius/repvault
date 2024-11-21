a9e2ad8c8fcc5f0b29a2faf6a5b564d13924092f
Antônio Vinícius <antoniovrdacosta@gmail.com>
2024-11-20 23:23:10 -0300

    fix: 🪲 Correct supabase file

--- src/supabase/database.types.ts
+++ src/supabase/database.types.ts
@@ -1,14 +1,3 @@
-Need to install the following packages:
-supabase@1.223.10
-Ok to proceed? (y) 
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
-
 export type Database = {
   public: {
     Tables: {
